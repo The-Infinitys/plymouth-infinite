3b3e30a3b038a8d51dc94791b71c2169d878de56
The-Infinitys <the.infinity.s.infinity@gmail.com>
2025-03-18 15:46:00 +0900

    画像ファイル名を変更し、ローディングSVGファイルを削除して新しい生成スクリプトを追加

--- image/generate-loading.py
+++ image/generate-loading.py
@@ -1,3 +1,4 @@
+"""
 <svg
     viewBox="0 0 120 60" version="1.1"
     xmlns="http://www.w3.org/2000/svg"
@@ -42,3 +43,52 @@
     </animateMotion>
   </circle>
 </svg>
+"""
+
+head="""<svg
+    viewBox="0 0 120 60" version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink">
+    <path
+    id="path"
+    d="
+    M 10,30
+    l 10,20
+    h 30
+    l 20,-40
+    h 30
+    l 10,20
+    l -10,20
+    h -30
+    l -20,-40
+    h -30
+    z
+    "
+    fill="none"
+    stroke="none"
+  />
+  <rect x="0" y="0" width="120" height="60" fill="#000" stroke="none"/>
+"""
+foot="""</svg>"""
+
+length = 10
+per=0.5
+r=5
+def circle(begin:float) -> str:
+    return f"""
+    <circle x="{120+2*r}" y="{60+2*r}" r="{r}" fill="#fff" stroke="none">
+    <animateMotion repeatCount="indefinite" dur="{str(length)}s" begin="{str(begin)}s">
+      <mpath href="#path" />
+    </animateMotion>
+  </circle>
+    """
+
+def generate_loading(level=100) -> str:
+    result = head
+    for i in range(level):
+        result += circle(i*length/level*per)
+    result += foot
+    return result
+  
+with open("loading.svg", "w") as f:
+    f.write(generate_loading())
