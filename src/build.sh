d9f877b02874692480b72e9f259fec4411c0c9af
The Infinity's <the.infinity.s.infinity@gmail.com>
2025-11-18 08:08:02 +0900

    ok

--- src/build.sh
+++ src/build.sh
@@ -19,7 +19,7 @@ SCRIPT_DIR=$(dirname "$0")
 cd "$SCRIPT_DIR/.."
 echo "プロジェクトのルートディレクトリに移動しました: $(pwd)"
 
-TOTAL_FRAME=360
+TOTAL_FRAME=90
 
 # --- 2. buildディレクトリのクリーンアップと作成 ---
 BUILD_DIR="./build"
